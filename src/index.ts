8f6700c098a3ed71d110f4cd41868ddb399425e7
Suzana Melo <suzanamelojor@gmail.com>
2022-02-10 21:32:52 +1300

    Stats project: ead csv file and parse the file using split and map to create an array of arrays for each line of data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import fs from 'fs';
+
+// fs = module from node to read the file
+const matches = fs
+  .readFileSync('football.csv', {
+    // tell what the content is, in this case a string, otherwise it will return a buffer
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+console.log(matches);
