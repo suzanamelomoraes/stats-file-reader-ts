648d98581d926f573c3ce3c2cd56a03277dce74c
Suzana Melo <suzanamelojor@gmail.com>
2022-02-13 18:22:50 +1300

    Stats project: refactor read to isolate the custom logic specific to football.csv file into mapRow method

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
-import { threadId } from 'worker_threads';
 
 // Tuple
 type MatchData = [Date, string, string, number, number, MatchResult, string];
@@ -22,17 +21,20 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6],
-        ];
-      });
+      .map(this.mapRow);
+  }
+
+  // isolated custom logic specific to football.csv file
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult,
+      row[6],
+    ];
   }
 }
 
