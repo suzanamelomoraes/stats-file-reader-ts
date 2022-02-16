01097072db8c6f0d71ed8cdbd07981bf1544edee
Suzana Melo <suzanamelojor@gmail.com>
2022-02-16 19:30:41 +1300

    Stats project: update index to use the new classes matchReader and CsvFileReader that uses composition to read the file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// Step 1 - while using the interface based's logic
+// Create an object that satisfies the ´DataReader'interface
+const csvFileReader = new CsvFileReader('football.csv');
 
-// Logic to show how many games Manchester United won
+// Step 2
+// Create an instance of MatchReader and pass in something that satisfies DataReader interface
+const matchReader = new MatchReader(csvFileReader);
+// Call the load method to execute the transformation and feed matches property
+matchReader.load();
+
+// Logic to test classes that shows how many games Manchester United won
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
