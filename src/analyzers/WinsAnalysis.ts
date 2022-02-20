f4486ef63b170e45d23d806939ee9c1cad72ee0c
Suzana Melo <suzanamelojor@gmail.com>
2022-02-20 14:29:48 +1300

    Stats project: create foldar for analysis files and include a class that checks how many games a givem team won

---
+++ src/analyzers/WinsAnalysis.ts
@@ -0,0 +1,24 @@
+import { MatchData } from '../MatchData';
+import { Analyzer } from '../Summary';
+import { MatchResult } from '../MatchResult';
+
+export class WinsAnalysis implements Analyzer {
+  constructor(public teamName: string) {}
+
+  run(matches: MatchData[]): string {
+    // Logic to test classes that shows how many games Manchester United won
+    let wins = 0;
+
+    for (let match of matches) {
+      if (match[1] === this.teamName && match[5] === MatchResult.HomeWin) {
+        wins++;
+      } else if (
+        match[2] === this.teamName &&
+        match[5] === MatchResult.AwayWin
+      ) {
+        wins++;
+      }
+    }
+    return `Team ${this.teamName} won ${wins} games`;
+  }
+}
